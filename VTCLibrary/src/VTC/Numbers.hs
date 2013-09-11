3e4745cfd32570bf35f046669a77cc8a41a692b0
Peter Chapin <PChapin@vtc.vsc.edu>
2013-09-11 19:29:53 -0400

    Cleaned up the implementation of computeCRC and isPrime.

--- VTCLibrary/src/VTC/Numbers.hs
+++ VTCLibrary/src/VTC/Numbers.hs
@@ -2,9 +2,7 @@ module VTC.Numbers(isPrime) where
 
 isPrime :: Int -> Bool
 isPrime n = not $ hasDivisor n $ n - 1
-             where hasDivisor _ 1 = False
-                   hasDivisor n trialNumber =
-                       if n `mod` trialNumber == 0
-                       then True
-                       else hasDivisor n $ trialNumber - 1
+            where hasDivisor _ 1 = False
+                  hasDivisor m trialNumber =
+                      m `mod` trialNumber == 0 || hasDivisor m (trialNumber - 1)
 
--- VTCLibrary/src/VTC/PNG.hs
+++ VTCLibrary/src/VTC/PNG.hs
@@ -1,7 +1,7 @@
 module VTC.PNG(hasPNGHeader, chunkCount, splitPNG, computeCRC, verifyCRC) where
 
-import Data.Bits
-import Data.Word
+import           Data.Bits
+import           Data.Word
 
 -- Returns True if the given list starts with a valid PNG signature.
 hasPNGHeader :: [Word8] -> Bool
@@ -19,30 +19,28 @@ splitPNG pngData = []
 
 
 -- Computes the CRC checksum over the given list using the algorithm in the PNG spec.
-processBit :: Word32 -> Word32
-processBit c =
-    if (testBit c 0)
-    then 0xEDB88320 `xor` (shift c (-1))
-    else shift c (-1)
-    
-bitLoop :: Int -> Word32 -> Word32
-bitLoop 0       c = processBit c
-bitLoop counter c = processBit $ bitLoop (counter - 1) c
-
-processByte :: Word32 -> Word32
-processByte c = bitLoop 7 c
-
-crcTable :: [Word32]
-crcTable = [ processByte n | n <- [0 .. 255]]
-
 computeCRC :: [Word8] -> Word32
-computeCRC rawData = (foldl update 0xFFFFFFFF rawData) `xor` 0xFFFFFFFF
+computeCRC rawData = complement $ foldl update 0xFFFFFFFF rawData
                      where update :: Word32 -> Word8 -> Word32
                            update c dataItem =
-                                  let cLowByte = (fromIntegral (c .&. 0x000000FF))::Word8
+                                  let cLowByte = fromIntegral (c .&. 0x000000FF)
                                       index    = cLowByte `xor` dataItem
-                                      intIndex = (fromIntegral index)::Int
-                                  in (crcTable !! intIndex) `xor` (shift c (-8))
+                                      intIndex = fromIntegral index
+                                  in (crcTable !! intIndex) `xor` shift c (-8)
+
+                           crcTable :: [Word32]
+                           crcTable = [ processByte n | n <- [0 .. 255]]
+                                      where processByte :: Word32 -> Word32
+                                            processByte c = bitLoop 7 c
+
+                                            bitLoop :: Int -> Word32 -> Word32
+                                            bitLoop 0       c = processBit c
+                                            bitLoop counter c = processBit $ bitLoop (counter - 1) c
+
+                                            processBit :: Word32 -> Word32
+                                            processBit c
+                                                | testBit c 0 = 0xEDB88320 `xor` shift c (-1)
+                                                | otherwise   = shift c (-1)
 
 
 -- Verifies the CRC checksum on each chunk in a list of chunks.
