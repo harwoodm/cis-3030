cd74ef68eeee46fcba15f0ef2d2c162f7f01242b
Peter Chapin <PChapin@vtc.vsc.edu>
2014-06-15 21:00:07 -0400

    Added a skeletal project for Scala samples.

---
+++ Scala/Hello/src/edu/vtc/cis3030/Hello.scala
@@ -0,0 +1,14 @@
+package edu.vtc.cis3030
+
+/**
+ * This object is a sandbox that can be used for experimenting with Scala code.
+ */
+object Hello {
+
+  /**
+   * The main method is the entry point to the program.
+   * @param args The program's command line arguments.
+   */
+  def main(args: Array[String]) = println("Hello, World!")
+
+}
