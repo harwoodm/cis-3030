8f495c65695ff1868ac67aa813fd4280d17026af
Peter Chapin <PChapin@vtc.vsc.edu>
2014-06-15 20:01:34 -0400

    Removed the Haskell PNG code from the cis-3030 samples. It is now in the Media project.

--- VTCLibrary/check/Check.hs
+++ VTCLibrary/check/Check.hs
@@ -7,7 +7,6 @@ import Test.Framework
 import {-@ HTF_TESTS @-} VTC.DFATest
 import {-@ HTF_TESTS @-} VTC.ListTest
 import {-@ HTF_TESTS @-} VTC.NumbersTest
-import {-@ HTF_TESTS @-} VTC.PNGTest
 
 main :: IO()
 main = htfMain htf_importedTests
--- VTCLibrary/check/VTC/PNGTest.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-module VTC.PNGTest where
-import VTC.PNG
-
-import Test.Framework
-
-test_pngCRC = do assertEqual          0 (computeCRC [])
-                 assertEqual  622876539 (computeCRC [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-                 assertEqual 3435948266 (computeCRC [0xFF, 0xFE, 0xFD, 0xFC, 0xFB, 0xFA, 0xF9, 0xF8])
